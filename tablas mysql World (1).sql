9d25fa83dc7776303939e14b6c4a831b44f329b3
Emily Muñoz <emilymunoz097@gmail.com>
2025-07-22 12:37:25 -0500

    mi primer commit

--- tablas mysql World (1).sql
+++ tablas mysql World (1).sql
@@ -25,7 +25,10 @@ values ('Lola', 18, 'lola@gmail.com', (date'2007-06-18')
 insert into user (nombre, edad, correo, fecha_nacimiento)
 values ('Rosa', 50, 'rosa@gmail.com', (date'1945-09-08')
 );
-
+insert into user (nombre, edad, correo, fecha_nacimiento)
+values ('Emily', 17, 'emilymunoz097@gmail.com', (date'2008-03-31')
+);
+/*
 update user set nombre = 'Lola' where id = 2;
 delete from user where id = 2;
 
@@ -113,4 +116,4 @@ create procedure pa_borrar_usuarios(
  end $
 delimiter ;
 CALL pa_borrar_usuarios (9);
-
+*/
