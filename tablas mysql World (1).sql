f34e7518fe256cde69fb2513864abfe4f57f0271
Sofia <ortegaobandoisabelsofia@gmail.com>
2025-07-22 12:28:34 -0500

    Mi primer commit con el insert de mi nombre.

--- tablas mysql World (1).sql
+++ tablas mysql World (1).sql
@@ -25,6 +25,10 @@ values ('Lola', 18, 'lola@gmail.com', (date'2007-06-18')
 insert into user (nombre, edad, correo, fecha_nacimiento)
 values ('Rosa', 50, 'rosa@gmail.com', (date'1945-09-08')
 );
+insert into user (nombre, edad, correo, fecha_nacimiento)
+values ('Sofia Ortega', 17, 'ortegaosofia@gmail.com', (date'2007-08-15')
+);
+/*
 
 update user set nombre = 'Lola' where id = 2;
 delete from user where id = 2;
@@ -113,4 +117,4 @@ create procedure pa_borrar_usuarios(
  end $
 delimiter ;
 CALL pa_borrar_usuarios (9);
-
+*/
